dc87c5a388fb14f7a36fc4e5ce7b7fca4749ae1f
Kundan Kumar <kundan.kumar@ekaaya.in>
2025-06-28 17:10:09 +0530

    code

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const mongoUrl= process.env.DB_URL;
 mongoose.connect(mongoUrl);
 
 const db = mongoose.connection;
-//add comment for checking
+//add comment for checking jh
 
 db.on('connected', () => console.log('✅ Mongoose connected'));
 db.on('error', (err) => console.error('❌ Mongoose error:', err));
