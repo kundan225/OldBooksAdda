1d5313518928672cc81c1fb4989e700887c013e5
Kundan Kumar <kundan.kumar@ekaaya.in>
2025-07-01 17:52:45 +0530

    code

--- routes/booksCategories.js
+++ routes/booksCategories.js
@@ -6,9 +6,6 @@ const BooksCategory = require('../models/BooksCategory');
 router.get('/', async (req, res) => {
   try {
     const data = await BooksCategory.findOne();
-    if (!data || !data.categories) {
-      return res.status(404).json({ message: 'No categories found' });
-    }
     res.status(200).json({ categories: data.categories }); // <-- wrap in object
   } catch (err) {
     res.status(500).json({ message: err.message });
